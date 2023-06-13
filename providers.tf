35c656fcf942bff914159719c77fc5455b2ea58c
priyabp85 <77617486+priyabp85@users.noreply.github.com>
2023-06-13 13:00:32 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,8 +5,13 @@ terraform {
       version = "3.61.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
-
 provider "azurerm" {
   # Configuration options
 }
